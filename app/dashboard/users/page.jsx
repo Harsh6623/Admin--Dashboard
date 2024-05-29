000a301cc188c213c284b9120461e3657031496b
unknown <harshborsadiya@gmail.com>
2024-05-29 11:01:42 +0530

    create a users page and add searchbar

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,5 +1,19 @@
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Link from "next/link";
+
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}></table>
+    </div>
+  );
 };
 
 export default UsersPage;
