ea12bf3f33f6335a5050c035c707ce3adadccf88
unknown <harshborsadiya@gmail.com>
2024-05-31 15:34:14 +0530

    add a server action into this project

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -1,17 +1,22 @@
+"use server";
+import { revalidatePath } from "next/cache";
 import { User } from "./models";
 import { connectToDB } from "./utils";
+import { redirect } from "next/navigation";
 
 export const addUser = async (formData) => {
-  "use server";
   const { username, email, password, phone, address, isAdmin, isActive } =
     Object.fromEntries(formData);
 
   try {
     connectToDB();
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
       phone,
       address,
       isAdmin,
@@ -22,4 +27,33 @@ export const addUser = async (formData) => {
     console.log(err);
     throw new Error("Failed to create user!");
   }
+
+  revalidatePath("/dashboard/users");
+  redirect("dashboard/users");
+};
+
+export const addProduct = async (formData) => {
+  const { title, desc, price, stock, color, size } =
+    Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+
+    const newProduct = new Product({
+      title,
+      desc,
+      price,
+      stock,
+      color,
+      size,
+    });
+
+    await newProduct.save();
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to create product!");
+  }
+
+  revalidatePath("/dashboard/products");
+  redirect("dashboard/products");
 };
